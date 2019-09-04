4858a8c9bb15b172a23612c4e6eaf3ec2f1ddd5e
Dylan <Dylan.Griff11@gmail.com>
2019-09-04 15:31:52 -0500

    added fetch data

--- src/scripts/concert.js
+++ src/scripts/concert.js
@@ -1,2 +1,6 @@
-
+fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=b2mgLaafqqPj3qedr8Zo6jklYkDTEYkb')
+  .then(main => main.json())
+  .then(parsedMain => {
+    console.table(parsedMain)
+  })
 
