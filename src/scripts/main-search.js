018cc55221b3b9dc12efe4002e05ea0beb8cc03e
Alex <o.a.momotyuk@gmail.com>
2019-09-04 13:47:50 -0500

    updated input values code

--- src/scripts/main-search.js
+++ src/scripts/main-search.js
@@ -17,22 +17,12 @@ searchInputs[2] = new searchItem( "meetups_input","meetups by topic","" )
 searchInputs[3] = new searchItem( "concerts_input","concerts by genre","" ) 
 
 document.querySelector('#search-button').addEventListener( "click", event => {
-
     for( let i = 0; i < searchInputs.length; i++ ) {
-
-        //let inputValue = document.querySelector("#parks_input").value;
         let inputValue = document.querySelector(`#${searchInputs[i].input}`).value;
-        if( searchInputs[i].input != inputValue ) {
+        if( searchInputs[i].initial != inputValue ) {
             searchInputs[i].query = inputValue;
         }
     }
-
-
-    /*
-    searchInputs[0].query = document.querySelector("#parks_input").value;
-    searchInputs[1].query = document.querySelector("#restaurants_input").value;
-    searchInputs[2].query = document.querySelector("#meetups_input").value;
-    searchInputs[3].query = document.querySelector("#concerts_input").value;*/
 })
 
 console.log( searchInputs );
