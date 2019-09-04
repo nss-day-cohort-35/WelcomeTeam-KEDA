a8008368dfd7e6b2fed0f34ea6b476a8462d4da5
WitheredFlame <witheredspam@gmail.com>
2019-09-04 16:07:59 -0500

    Added required connectivity

--- src/scripts/park.js
+++ src/scripts/park.js
@@ -2,10 +2,10 @@
 
 
 
-    getParkData = function(query) { //get data
+    getParkData = function() { //get data
         
     
-       fetch(`https://data.nashville.gov/resource/74d7-b74t.json?notes=${query}`) //get data based of off search, aka parksearch bar's text content
+       fetch(`https://data.nashville.gov/resource/74d7-b74t.json`) //get data based of off search, aka parksearch bar's text content
        .then(entrieslist => entrieslist.json()) // get and parse data
        .then(parsedentries =>{
 
@@ -26,16 +26,31 @@ function updateParkSearchResults(idata) { // works with the park api, so this is
     parktargetinsert.innerHTML = ``;
     //clear results in park section to make room for the loop inserting the new ones
 
+    let workinglist = [];
 
-    if ( idata.length === 0 ) {
-        idata[0] = {park_name:"No park"}
+    workinglist[0] = {park_name:"No park"} // i recomend you add a "no option" to the start of your list
+
+    for (let i = 0; i < idata.length; i++) {
+
+        if (idata[i].notes.includes(searchInputs[0].query)) {
+            workinglist.push(idata[i]);
+        }
+
     }
 
-    for (let i = 0; i < idata.length; i++) { // loop through all results, creating html framework as we go
+
+
+
+
+
+
+
+
+    for (let i = 0; i < workinglist.length; i++) { // loop through all results, creating html framework as we go
         parktargetinsert.innerHTML += `
         <div class="inline">
         <button class="pcheckbutton styleasbutton" id = "pbutton${i}"></button>
-        <p id = "ptarget${i}">${idata[i].park_name}</p>
+        <p id = "ptarget${i}">${workinglist[i].park_name}</p>
         </div>
         
         
