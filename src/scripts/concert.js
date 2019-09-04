ec12c363cb1f40c7a168843c707bf50b4856881a
Dylan <Dylan.Griff11@gmail.com>
2019-09-04 15:58:44 -0500

    concert.js changes

--- src/scripts/concert.js
+++ src/scripts/concert.js
@@ -1,15 +1,2 @@
-<<<<<<< HEAD
 fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=b2mgLaafqqPj3qedr8Zo6jklYkDTEYkb')
-=======
-fetch("https://app.ticketmaster.com/discovery/v2/")
->>>>>>> master
-  .then(main => main.json())
-  .then(parsedMain => {
-    console.table(parsedMain)
-  })
-<<<<<<< HEAD
-=======
-
-
->>>>>>> master
 
