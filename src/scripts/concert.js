415d20f6a4625fa284f006ee132d7e3b9dd93354
Dylan <Dylan.Griff11@gmail.com>
2019-09-05 09:40:48 -0500

    updated the fetch call

--- src/scripts/concert.js
+++ src/scripts/concert.js
@@ -1,2 +1,5 @@
-fetch('https://app.ticketmaster.com/discovery/v2/events.json?apikey=b2mgLaafqqPj3qedr8Zo6jklYkDTEYkb')
-
+fetch('https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=31apikey=b2mgLaafqqPj3qedr8Zo6jklYkDTEYkb')
+  .then(music => music.json())
+  .then(parsedMain => {
+    console.table(parsedMain)
+  })
