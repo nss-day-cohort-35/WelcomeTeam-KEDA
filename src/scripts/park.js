d72c46d5d0b951f205e07871420816dfb0346414
Dylan <Dylan.Griff11@gmail.com>
2019-09-05 15:11:10 -0500

    console.log my api

--- src/scripts/park.js
+++ src/scripts/park.js
@@ -2,27 +2,48 @@
 
 
 
-    getParkData = function() { //get data
+    getParkData = function(query) { //get data
         
     
-       fetch(`https://data.nashville.gov/resource/74d7-b74t.json`) //get data based of off search, aka parksearch bar's text content
-       .then(entrieslist => entrieslist.json()) // get and parse data
-       .then(parsedentries =>{
+
+        if (query === "") {
+            updateParkSearchResults([]);
+       
+         } else if (query === "all" || query === "park" || query === "parks") {
+
+            searchInputs[0].query = "";
+
+            fetch(`https://data.nashville.gov/resource/74d7-b74t.json`) //get data based of off search, aka parksearch bar's text content
+            .then(entrieslist => entrieslist.json()) // get and parse data
+            .then(parsedentries =>{
+
+            updateParkSearchResults(parsedentries); // send data off
+            });
+
+         } else {
+
+            fetch(`https://data.nashville.gov/resource/74d7-b74t.json`) //get data based of off search, aka parksearch bar's text content
+            .then(entrieslist => entrieslist.json()) // get and parse data
+            .then(parsedentries =>{
 
             updateParkSearchResults(parsedentries); // send data off
 
-       });
-       
-      
+            });
+
+         }
        
     }
+
+
 
 
 function updateParkSearchResults(idata) { // works with the park api, so this is not universal. Uses the (letter)target(number) naming conventiion
 
     let parktargetinsert = document.querySelector("#parkresults"); // the park results container
     //you can change this to the id of the container for park search results, please still keep it outputing to the park results section
+
     console.log(idata);
+
     parktargetinsert.innerHTML = ``;
     //clear results in park section to make room for the loop inserting the new ones
 
@@ -37,10 +58,6 @@ function updateParkSearchResults(idata) { // works with the park api, so this is
         }
 
     }
-
-
-
-
 
 
 
